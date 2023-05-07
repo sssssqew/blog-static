dea57ec4f6e964ee82b87b20ffd02bf717169ae9
sssssqew <sunrise.in.my.dream@gmail.com>
2023-05-07 20:28:26 +0900

    added header, footer, avatar, notification for home page

---
+++ js/home.js
@@ -0,0 +1,20 @@
+const scroller = new Scroller(false)
+
+window.addEventListener("load", (event) => {
+  // 테마변경 (다크모드/ 일반모드)
+  const mode = document.querySelector('.mode')
+  const icons = mode.querySelectorAll('.fa-solid')
+  const header = document.querySelector('header')
+  const footer = document.querySelector('.footer')
+
+  mode.addEventListener('click', (event) => {
+    document.body.classList.toggle('dark')
+    header.classList.toggle('dark')
+    
+    for(const icon of icons){
+      icon.classList.contains('active') ? 
+        icon.classList.remove('active') 
+        : icon.classList.add('active')
+    }
+  })
+})
