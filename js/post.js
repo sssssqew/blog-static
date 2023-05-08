0d623920fe24652384d22ff82e1c0850f0988e80
sssssqew <sunrise.in.my.dream@gmail.com>
2023-05-09 00:58:14 +0900

    added input tag for post page

--- js/post.js
+++ js/post.js
@@ -1,7 +1,24 @@
+// textarea 자동높이 조절 
 const postContents = document.querySelector('.post-container .post-contents textarea')
 
 // keydown, keyup 으로 하면 깜빡임 현상 발생
 postContents.addEventListener('input', function(event){ // this 를 사용하기 위하여 function 키워드로 정의함 
   this.style.height = 'auto' // textarea 높이 초기화 
   this.style.height = this.scrollHeight + 'px' // 현재 사용자가 입력중인 스크롤높이로 textarea 높이 설정
+})
+
+// 태그입력 기능
+const tagList = document.querySelector('.post-container .post-tags ul')
+const tagInput = document.querySelector('.post-container .post-tags input') 
+const tagslimit = 10 // 태그 갯수 제한
+const tagLength = 10 // 태그 글자수 제한
+
+tagInput.addEventListener('keyup', function(event){
+  console.log('태그 입력중...', event.key)
+  if(event.key === 'Enter' && this.value.trim() !== '' && this.value.trim().length <= tagLength && tagList.children.length < tagslimit){
+    const list = document.createElement('li')
+    list.innerHTML = `#${this.value.trim()}<a href='#'>x</a>`
+    tagList.appendChild(list)
+    this.value = ''
+  }
 })
