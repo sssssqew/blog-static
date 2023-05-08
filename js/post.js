87578edc70d74196cb15d30f6ee701a147012a01
sssssqew <sunrise.in.my.dream@gmail.com>
2023-05-08 23:12:08 +0900

    added post title, post contents for post page

--- js/post.js
+++ js/post.js
@@ -1,1 +1,7 @@
-console.log('post page')
+const postContents = document.querySelector('.post-container .post-contents textarea')
+
+// keydown, keyup 으로 하면 깜빡임 현상 발생
+postContents.addEventListener('input', function(event){ // this 를 사용하기 위하여 function 키워드로 정의함 
+  this.style.height = 'auto' // textarea 높이 초기화 
+  this.style.height = this.scrollHeight + 'px' // 현재 사용자가 입력중인 스크롤높이로 textarea 높이 설정
+})
